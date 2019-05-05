313c2462a4f7bfefb27acbe07f6d49e4ce3208ad
kotek <aviv.kotek@cs.huji.ac.il>
2019-05-05 14:31:12 +0300

    fix tests, uax_email tok impl, stopwords tok impl

--- PycharmProjects/CDN/preprocessing/tokenizer.py
+++ PycharmProjects/CDN/preprocessing/tokenizer.py
@@ -1,52 +1,75 @@
 import re
-class Tokenizer:
+from enum import Enum, auto
+from collections import OrderedDict
+from nltk.corpus import stopwords
 
+
+class Tokenizer:
     # Tokenizer names and behavior is borrowed from Elasticsearch,
     # Tokenizer's will behave as same as analyzers in ES, standard_tokenizer <-> ES standard_analyzer
 
-    # PUNCTUATION:
+    # ENUMS:
+    # ------------------
+    class TokenizerType(Enum):
+        STANDARD = auto()
+        UAX_EMAIL = auto()
+
+    # ------------------
+
+    # PUNCTUATION MARKS:
     # ------------------
     BASIC_PUNCTS = [',', ':', ';', '?', '!', '/', '?', '(', ')', '...']
-    MEANINGFUL_PUNCTS = ["'", '-', "_"]
-    PUNCT_MARKS = BASIC_PUNCTS + MEANINGFUL_PUNCTS
-    GENERAL_TYPOGRPHY = ['&', '*', '@', '^', '=', '#', ':', '~']
-    WORD_DIVIDERS = [" ", '.'] + GENERAL_TYPOGRPHY + ['$']
+    MEANINGFUL_PUNCTS = ["'", "_", "."]
+    GENERAL_TYPOGRPHY = ['&', '*', '^', '=', '#', ':', '~', '@']
+    WORD_DIVIDERS = [" ", "-", "$"] + GENERAL_TYPOGRPHY
+    PUNCT_MARKS = BASIC_PUNCTS + MEANINGFUL_PUNCTS + WORD_DIVIDERS
     # ------------------
     # ANALYZERS:
     LEGAL_TOKEN_CHARS = ["letter", "digit", "whitespace", "punctuation", "symbol"]
     # ------------------
+    # STOPWORDS:
+    ENG_STOPS = set(stopwords.words('english'))
+    # ------------------
+    # ------------------
     # REGEX:
-    REGEX_NOT_WORD, REGEX_NOT_DIGIT, REGEX_NOT_WHITESPACE = "[^a-zA-Z]", "[^0-9]", "\S"
-    REGEX_NOT_PUNCTUATION = "[^!#$%&'()*+,-./:;<=>?@[\]^_`{|}~]"
+    REGEX_NOT_WORD, REGEX_NOT_DIGIT, REGEX_NOT_WHITESPACE = "[^a-zA-Z]", "[^0-9]", "\S"  # [^ indicates NOT
+    REGEX_NOT_PUNCTUATION, REGEX_EMAIL = "[^!#$%&'()*+,-./:;<=>?@[\]^_`{|}~]", "[\w\.-]+@[\w\.-]+"
+
     # ------------------
 
     def __init__(self) -> None:
         super().__init__()
 
+    def general_tokenizer(self, s: str, word_divider: list, meaningful: list) -> list:
+
+        # General tokenizing algorithm,
+        # iterates on given string, splits on given and adds tokens
+
+        tokens, chars = OrderedDict(), list()
+        s = s.strip()
+
+        for i in range(len(s)):
+            if s[i] in word_divider:
+                if chars:
+                    tokens["".join(chars)], chars = len(tokens), []  # value is the order of insertion
+            elif s[i] in self.PUNCT_MARKS:
+                if s[i] in meaningful and i != len(s) - 1 and (s[i + 1].isalpha() or s[i + 1].isnumeric()):
+                    chars.append(s[i])
+            else:
+                chars.append(s[i])
+
+        if chars: tokens["".join(chars)] = len(tokens)
+        return list(tokens.keys())
+
     def standard_tokenizer(self, s: str) -> list:
 
-        # The standard tokenizer provides grammar based tokenization (based on the Unicode Text Segmentation algorithm)
-        # it removes whitespaces, punctuation, duplications and lower cases tokens ('lowercase' filter)
+        # The standard tokenizer provides grammar based tokenization,
+        # it removes whitespaces, punctuation, duplications and lower cases tokens
 
         # "The 2 QUICK Brown-Foxes jumped over the lazy dog's bone."
         # >> [ the, 2, quick, brown, foxes, jumped, over, the, lazy, dog's, bone ]
-
-        tokens, chars = dict(), list()
-        s = s.lower().strip()
-
-        for i in range(len(s)):
-            if s[i] in self.WORD_DIVIDERS:
-                if chars:
-                    tokens[len(tokens)] = "".join(chars)  # keys are order of insertion
-                    chars = []
-            elif s[i] in self.PUNCT_MARKS:
-                if s[i] in self.MEANINGFUL_PUNCTS and i != len(s) - 1:
-                    if s[i + 1].isalpha():
-                        chars.append(s[i])
-            else:
-                chars.append(s[i])
-        if chars: tokens[len(tokens)] = "".join(chars)
-        return [tokens[key] for key in sorted(tokens.keys())]
+        assert s is not None
+        return self.general_tokenizer(s.lower(), *self.__build_punct(self.TokenizerType.STANDARD))
 
     def keyword_tokenizer(self, s: str) -> list:
 
@@ -57,19 +80,6 @@ class Tokenizer:
         # >> [ New York ]
 
         return [s.lower()]
-
-    def _gen_reg_from_token_chars(self, sep_lst: list) -> str:
-        separators = list()
-        for sep in sep_lst:
-            if sep == "letter":
-                separators.append(self.REGEX_NOT_WORD)
-            elif sep == "digit":
-                separators.append(self.REGEX_NOT_DIGIT)
-            elif sep == "whitespace":
-                separators.append(self.REGEX_NOT_WHITESPACE)
-            elif sep == "punctuation" or sep == "symbol":
-                separators.append(self.REGEX_NOT_PUNCTUATION)
-        return "|".join(separators) # create an OR regex
 
     def edge_ngram_tokenizer(self, s: str, min_gram: int = 1, max_gram: int = 2, tok_chars: list = []) -> list:
 
@@ -87,12 +97,12 @@ class Tokenizer:
         # >> [Q, Qu, F, Fo]
 
         assert s is not None
-        self._assert_grams(min_gram, max_gram, tok_chars)
+        self.__assert_grams(min_gram, max_gram, tok_chars)
 
-        split_regex = self._gen_reg_from_token_chars(tok_chars)
+        split_regex = self.__gen_reg_from_token_chars(tok_chars)
         splitted = re.split(split_regex, s) if split_regex else [s]
-        tokens = list()
 
+        tokens = list()
         for word in splitted:
             cur_gram, chars, cur_idx = min_gram, list(), 0
             while cur_idx < cur_gram <= max_gram and cur_gram <= len(word):
@@ -102,11 +112,10 @@ class Tokenizer:
                     tokens.append("".join(chars))
                     chars, cur_idx = list(), 0
                     cur_gram += 1
-
         return tokens
 
     def ngram_tokenizer(self, s: str, min_gram: int = 1, max_gram: int = 2, tok_chars: list = []) -> list:
-
+        # TODO impl this
         # The ngram tokenizer first breaks text down into words whenever it encounters words from token-chars,
         # then it emits N-grams of each word of the specified length.
         # N-grams are like a sliding window that moves across the word -
@@ -119,12 +128,79 @@ class Tokenizer:
         # "Quick Fox"
         # >> [ Q, Qu, u, ui, i, ic, c, ck, k, "k ", " ", " F", F, Fo, o, ox, x ]
 
-        self._assert_grams(min_gram, max_gram, tok_chars)
+        self.__assert_grams(min_gram, max_gram, tok_chars)
 
         return []
 
-    def _assert_grams(self, min_gram, max_gram, tok_chars):
+    def uax_url_email_tokenizer(self, s: str) -> list:
 
+        # The uax_url_email tokenizer is like the standard tokenizer except that
+        # it recognises URLs and email addresses as single tokens.
+
+        # "Email me at john.smith@global-international.com"
+        # >> [ Email, me, at, john.smith@global-international.com ]
+        # while standard tokenizer >> [ email, me, at, john.smith, global, international.com ]
+        assert s is not None
+        if not re.findall(self.REGEX_EMAIL, s):
+            return self.standard_tokenizer(s)
+        return self.general_tokenizer(s, *self.__build_punct(self.TokenizerType.UAX_EMAIL))
+
+    def stop_words_tokenizer(self, s: str, sw_file=None, stops=ENG_STOPS, rmv_trail=True) -> list:
+
+        # The stopwords tokenizer removes stopwords from a given string,
+        # it defaults to using the _english_ stop words.
+
+        # sw_file is a path to a stopwords file configuration.
+        # each stop word should be in its own "line" (separated by a line break)
+
+        # rmv_trailing set to false in order to not ignore the last term of a search if it is a stop word.
+        # useful while using edge_ngram as a query like green a can be extended to green apple
+
+        # "The 2 QUICK Brown-Foxes jumped over the lazy dog's bone."
+        # >> [2, QUICK, Brown-Foxes, jumped, lazy, dog's, bone.]
+
+        if sw_file:
+            stops = set()
+            with open(sw_file, 'r') as f:
+                for line in f:
+                    stops.add(line.rstrip())
+                return self.__stop_words_remover(s, stops, rmv_trail)
+        return self.__stop_words_remover(s, stops, rmv_trail)
+
+    # Helper private methods:
+    # ----------------------------------------------------
+    def __stop_words_remover(self, s: str, stops: set, rmv_trail=True):
+
+        assert s is not None, stops is not None
+
+        s = s.strip().split(" ")
+        tokens = [tok for tok in s if tok.lower() not in stops]
+
+        if not rmv_trail and s[-1] in stops: tokens.append(s[-1])
+        return tokens
+
+    def __gen_reg_from_token_chars(self, sep_lst: list) -> str:
+        separators = list()
+        for sep in sep_lst:
+            if sep == "letter":
+                separators.append(self.REGEX_NOT_WORD)
+            elif sep == "digit":
+                separators.append(self.REGEX_NOT_DIGIT)
+            elif sep == "whitespace":
+                separators.append(self.REGEX_NOT_WHITESPACE)
+            elif sep == "punctuation" or sep == "symbol":
+                separators.append(self.REGEX_NOT_PUNCTUATION)
+        return "|".join(separators)  # create an OR regex
+
+    def __build_punct(self, tok_type: TokenizerType) -> tuple:
+        if tok_type == self.TokenizerType.UAX_EMAIL:
+            dividers = list(filter(lambda s: s not in ['@', '-', '.'], self.WORD_DIVIDERS))
+            meaningful = self.MEANINGFUL_PUNCTS + ['@', '-']
+            return dividers, meaningful
+        return self.WORD_DIVIDERS, self.MEANINGFUL_PUNCTS
+
+    def __assert_grams(self, min_gram, max_gram, tok_chars):
         assert 0 < min_gram < 25, 0 < max_gram < 25
         assert min_gram <= max_gram
         assert all(tokens in self.LEGAL_TOKEN_CHARS for tokens in tok_chars)
+    # ----------------------------------------------------
--- PycharmProjects/CDN/tests/testtokenisation.py
+++ PycharmProjects/CDN/tests/testtokenisation.py
@@ -1,4 +1,4 @@
-import unittest, random
+import unittest, random, os
 from preprocessing.tokenizer import *
 
 
@@ -10,29 +10,29 @@ class TestTokenisation(unittest.TestCase):
 
     def test_standard_tokenizer(self):
         # removes word dividers, duplications and lowercase  words
-        str1 = " i'm A simp le string. string  "
+        str1 = " i'm A simp le string. . string  "
 
         tokens = self.tokenizer.standard_tokenizer(str1)
 
-        self.assertEqual(len(str1.split()) - 1, len(tokens))  # duplications
-        self.assertEqual("i'm", tokens[0])
-        self.assertEqual("a", tokens[1])
-        self.assertEqual("string", tokens[-1])
+        self.assertEqual(5, len(tokens))  # duplications
+        self.assertListEqual(["i'm", 'a', 'simp', 'le', 'string'], tokens)
 
     def test_standard_tokenizer_punct(self):
         # removes punctuation
         rnd1, rnd2 = random.choice(self.tokenizer.PUNCT_MARKS), random.choice(self.tokenizer.PUNCT_MARKS)
-        str1 = "don$t (you) ever_speak" + rnd1 + "^to' him! you-bad-boy" + rnd2
+        str1 = "don$t (you) ever_5speak" + rnd1 + "^to' him! you-bad-boy" + rnd2
 
         tokens = self.tokenizer.standard_tokenizer(str1)
 
-        self.assertEqual(7, len(tokens))  # typography should split words
-        self.assertTrue(tokens[0] == "don"
-                        and tokens[1] == "t"
-                        and tokens[2] == "you"
-                        and tokens[3] == "ever_speak"
-                        and tokens[-1] == "you-bad-boy")
+        self.assertEqual(8, len(tokens))  # typography should split words
+        self.assertListEqual(['don', 't', 'you', 'ever_5speak', 'to', 'him', 'bad', 'boy'], tokens)
         self.assertTrue(all(punct not in tokens for punct in self.tokenizer.PUNCT_MARKS))
+
+    def test_standard_tokenizer_invalid(self):
+        str2 = random.randint(1, 5) * " "
+
+        tokens = self.tokenizer.standard_tokenizer(str2)
+        self.assertEqual(0, len(tokens))
 
     def test_keyword_tokenizer(self):
         str1 = "Kfar Saba city"
@@ -60,16 +60,63 @@ class TestTokenisation(unittest.TestCase):
         self.assertListEqual(['Qui', 'Quic', 'Quick', 'Bro', 'Brow', 'Brown', 'Fox'], tokens)
 
     def test_edge_ngram_tokenizer_invalid(self):
-
-        self.assertRaises(AssertionError, self.tokenizer.edge_ngram_tokenizer, None)
-
-        # check empty strings:
-        str1 = ""
-
-        tokens = self.tokenizer.edge_ngram_tokenizer(str1)
-        self.assertEqual(0, len(tokens))
-
-        str2 = str1 + " "
+        str2 = " "  # len == 1
 
         tokens = self.tokenizer.edge_ngram_tokenizer(str2, min_gram=2, max_gram=2)
         self.assertEqual(0, len(tokens))
+
+    def test_uax_url_email_tokenizer(self):
+        str1 = "Email me at john.smith@global-international.com"
+
+        tokens = self.tokenizer.uax_url_email_tokenizer(str1)
+
+        self.assertEqual(4, len(tokens))
+        self.assertListEqual(['Email', 'me', 'at', 'john.smith@global-international.com'], tokens)
+
+    def test_uax_url_email_tokenizer_invalid(self):
+        # should behave like standard tokenizer
+        str1 = "Email me at john.smith.global-international.com"
+
+        tokens = self.tokenizer.uax_url_email_tokenizer(str1)
+
+        self.assertEqual(5, len(tokens))
+        self.assertListEqual(['email', 'me', 'at', 'john.smith.global', 'international.com'], tokens)
+
+    def test_stopwords_tokenizer(self):
+        # should JUST remove stopwords without any tokenization process
+        str1 = "The 2 QUICK Brown-Foxes jumped over the lazy dog's bone."
+
+        tokens = self.tokenizer.stop_words_tokenizer(str1)
+
+        self.assertEqual(7, len(tokens))
+        self.assertListEqual(['2', 'QUICK', 'Brown-Foxes', 'jumped', 'lazy', "dog's", 'bone.'], tokens)
+
+    def test_stopwords_tokenizer_from_file(self):
+        str1 = "im awake or asleep a"
+
+        path = os.path.join(os.path.dirname(__file__), "files\stops.txt")
+        tokens = self.tokenizer.stop_words_tokenizer(str1, path, None, False)
+
+        self.assertEqual(4, len(tokens))
+        self.assertListEqual(['awake', 'or', 'asleep', 'a'], tokens)
+
+    def test_stopwords_invalidity(self):
+        str1 = random.randint(1, 5) * " "
+
+        tokens = self.tokenizer.stop_words_tokenizer(str1)
+
+        self.assertListEqual([""], tokens)  # strips and splits on " "
+
+
+    def test_invalidity(self):
+        # tests simple invalid inputs for all existing tokenizers
+        funcs_list = [self.tokenizer.standard_tokenizer, self.tokenizer.edge_ngram_tokenizer,
+                      self.tokenizer.uax_url_email_tokenizer]
+
+        str1 = ""
+        for func in funcs_list:
+
+            self.assertRaises(AssertionError, func, None)
+
+            tokens = func(str1)
+            self.assertEqual(0, len(tokens))
