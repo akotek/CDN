d1fa5381235e1520e0c07e7e93ed52e1ebde0cca
kotek <kotekmath@gmail.com>
2019-06-14 15:31:23 +0300

    .

--- PycharmProjects/CDN/preprocessing/tokenize/keyword.py
+++ PycharmProjects/CDN/preprocessing/tokenize/keyword.py
@@ -3,11 +3,11 @@ from preprocessing.tokenize.tokenizer import Tokenizer
 
 class KeywordTokenizer(Tokenizer):
 
-    def tokenize(self, s: str) -> list:
+    def tokenize(self, s: str, stops: set = None) -> list:
         # The keyword tokenizer is a “noop” tokenizer that accepts whatever text it
         #  is given and outputs the exact same text as a single term
 
         # "New York"
         # >> [ New York ]
 
-        return [s.lower()]
+        return [(s, 1)]
